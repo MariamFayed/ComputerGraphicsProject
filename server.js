e77f8e26a12ebb70f59db6a8bf4388a91321c7bf
Mariam Fayed <mariammsalahh@gmail.com>
2022-01-08 19:42:19 +0200

    updated port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const app = express();            
-const port = 5500;                 
+var port = process.env.PORT || 5000;                
 
 app.get('/', (req, res) => {       
     res.sendFile('index.html', {root: __dirname});    
