0075badfee56bd4e720bc2df020778222522d8aa
Mariam Fayed <mariammsalahh@gmail.com>
2022-01-08 19:50:55 +0200

    moved dirs to public directory

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();            
 var port = process.env.PORT || 5000;                
 
-app.use(express.static('index.js'));   
-                                        
+app.use(express.static('public')); 
 
 app.listen(port, () => {    
     console.log(`Now listening on port ${port}`); 
